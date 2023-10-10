0f19d4cb8441b8d2866de0c7af5b6b6b11946960
Mubeen142 <mubeenullah323@gmail.com>
2023-10-10 20:37:27 +0200

    Update updates.blade.php

--- updates.blade.php
+++ updates.blade.php
@@ -2,6 +2,49 @@
 
 @section('container')
 <div class="card mb-6">
+    @if(Cache::has('app_updating'))
+    <div class="card-body">
+      <div class="empty-state" data-height="400" style="height: 400px;">
+          <div class="empty-state-icon" style="background-color: #059669; display: flex; justify-content: center; align-items: center">
+            <img src="/assets/src/spinners/blocks.svg" style="width: 50px;">
+          </div>
+          <h2>Installing {{ Cache::get('app_updating')['type'] }} v{{ Cache::get('app_updating')['version'] }}</h2>
+          <p class="lead">
+            <span id="progress">{{ Cache::get('app_updating')['progress'] }}</span><span id="loading">...</span>
+          </p>
+          <a href="https://wemx.net/news" target="_blank" class="mt-4 bb">Update Manually</a>
+        </div>
+    </div>
+    <script>
+      let count = 0;
+      const loadingElement = document.getElementById('loading');
+  
+      setInterval(() => {
+          count++;
+          loadingElement.textContent = '.'.repeat(count % 4);
+      }, 500);
+
+      const progressElement = document.getElementById('progress');
+      setInterval(async () => {
+        try {
+            let response = await fetch('{{ route("updates.progress") }}');
+            
+            // Check if the response is successful
+            if (!response.ok) {
+                throw new Error(`Failed to load update progress, please wait 2 min or perform a manual update.`);
+            }
+
+            let data = await response.json();
+            if(data.updating) {
+              progressElement.innerHTML = data.progress;
+            }
+        } catch (error) {
+            progressElement.innerHTML = error;
+            console.error("There was an error fetching the data:", error);
+        }
+    }, 5000);
+  </script>
+    @else
     @if(config('app.version') >= $latest_version->version)
     <div class="card-body">
         <div class="empty-state" data-height="400" style="height: 400px;">
@@ -27,10 +70,11 @@
           <p class="lead">
            You are running an outdated version of WemX
           </p>
-          <a href="{{ route('updates.install', ['version' => $latest_version->version, 'type' => 'stable']) }}" class="btn btn-success mt-4">Install v{{ $latest_version->version }}</a>
+          <button type="button" data-toggle="modal" data-target="#installUpdateModal" class="btn btn-success mt-4">Install v{{ $latest_version->version }}</button>
           <a href="https://wemx.net/news" target="_blank" class="mt-4 bb">View Changelog</a>
         </div>
     </div>
+    @endif
     @endif
 </div>
 
@@ -52,5 +96,29 @@
         </div>
         @endforeach
     </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="installUpdateModal" tabindex="-1" role="dialog" aria-labelledby="installUpdateModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="installUpdateModalLabel">Installing Update {{ $latest_version->version }}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="alert alert-info" role="alert">
+          It's important that you create a backup of files as well as the database if an unforeseen event is to happen.
+        </div>
+        {!! $latest_version->changelog !!}
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+        <a href="{{ route('updates.install', ['version' => $latest_version->version, 'type' => 'stable']) }}" class="btn btn-success">Install {{ $latest_version->version }}</a>
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
