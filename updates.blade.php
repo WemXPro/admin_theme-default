c52a1ea7921fd8e4befbb73467d378dfed587e45
GIGABAIT93 <59637348+GIGABAIT93@users.noreply.github.com>
2023-10-11 14:38:58 +0300

    Update updates.blade.php

--- updates.blade.php
+++ updates.blade.php
@@ -8,17 +8,17 @@
           <div class="empty-state-icon" style="background-color: #059669; display: flex; justify-content: center; align-items: center">
             <img src="/assets/src/spinners/blocks.svg" style="width: 50px;">
           </div>
-          <h2>Installing {{ Cache::get('app_updating')['type'] }} {{ Cache::get('app_updating')['version'] }}</h2>
+          <h2>{{ __('admin.installing') }} {{ Cache::get('app_updating')['type'] }} {{ Cache::get('app_updating')['version'] }}</h2>
           <p class="lead">
             <span id="progress">{{ Cache::get('app_updating')['progress'] }}</span><span id="loading">...</span>
           </p>
-          <a href="https://wemx.net/news" target="_blank" class="mt-4 bb">Update Manually</a>
+          <a href="https://wemx.net/news" target="_blank" class="mt-4 bb">{{ __('admin.update_manually') }}</a>
         </div>
     </div>
     <script>
       let count = 0;
       const loadingElement = document.getElementById('loading');
-  
+
       setInterval(() => {
           count++;
           loadingElement.textContent = '.'.repeat(count % 4);
@@ -28,7 +28,7 @@
       setInterval(async () => {
         try {
             let response = await fetch('{{ route("updates.progress") }}');
-            
+
             // Check if the response is successful
             if (!response.ok) {
                 throw new Error(`Failed to load update progress, please wait 2 min or perform a manual update.`);
@@ -51,15 +51,15 @@
             <div class="empty-state-icon" style="background-color: #059669;">
               <i class="fas fa-check"></i>
             </div>
-            <h2>You can running the latest version ({{config('app.version')}})</h2>
+            <h2>{{ __('admin.running_latest_version') }} ({{config('app.version')}})</h2>
             <p class="lead">
-              Your WemX application is up-to-date! New updates will appear here.
+              {{ __('admin.running_latest_version_desc') }}
             </p>
-            <a href="https://wemx.net/news" target="_blank" class="btn btn-success mt-4">Latest News</a>
+            <a href="https://wemx.net/news" target="_blank" class="btn btn-success mt-4">{{ __('client.latest_news') }}</a>
             @if(config('app.version') == 'dev')
-              <a href="{{ route('updates.install', ['version' => 'latest', 'type' => 'dev']) }}" class="btn btn-success mt-4">Install Dev version</a>
+              <a href="{{ route('updates.install', ['version' => 'latest', 'type' => 'dev']) }}" class="btn btn-success mt-4">{{ __('admin.install_dev_version') }}</a>
             @endif
-            <a href="{{ route('updates.index') }}" class="mt-4 bb">Refresh</a>
+            <a href="{{ route('updates.index') }}" class="mt-4 bb">{{ __('admin.refresh') }}</a>
           </div>
     </div>
     @else
@@ -69,12 +69,12 @@
             <i class="fas fa-download"></i>
             {{-- <img src="/assets/src/spinners/blocks.svg" style="width: 50px;"> --}}
           </div>
-          <h2>Update Available</h2>
+          <h2>{{ __('admin.update_available') }}</h2>
           <p class="lead">
-           You are running an outdated version of WemX
+           {{ __('admin.update_available_desc') }}
           </p>
-          <button type="button" data-toggle="modal" data-target="#installUpdateModal" class="btn btn-success mt-4">Install v{{ $latest_version->version }}</button>
-          <a href="https://wemx.net/news" target="_blank" class="mt-4 bb">View Changelog</a>
+          <button type="button" data-toggle="modal" data-target="#installUpdateModal" class="btn btn-success mt-4">{{ __('admin.install') }} v{{ $latest_version->version }}</button>
+          <a href="https://wemx.net/news" target="_blank" class="mt-4 bb">{{ __('admin.view_changelog') }}</a>
         </div>
     </div>
     @endif
@@ -87,14 +87,14 @@
         <div class="media mb-4">
             <img class="mr-3" src="https://imgur.com/oJDxg2r.png" alt="wemx logo" style="width: 46px; height: 46px; border-radius: 10px">
             <div class="media-body">
-              <h5 class="mt-0">Version {{ $version->version }} ({{ $version->type }})</h5>
+              <h5 class="mt-0">{{ __('admin.version') }} {{ $version->version }} ({{ $version->type }})</h5>
               <p class="mb-0">
-                {!! $version->changelog ?? 'No changelog provided' !!}
+                {!! $version->changelog ?? __('admin.no_changelog_provided') !!}
               </p>
               <small>{{ Carbon::parse($version->created_at)->diffForHumans() }} ({{ Carbon::parse($version->created_at)->format('d M Y') }})</small>
             </div>
             @if(config('app.version') == $version->version)
-              <a href="#" class="btn btn-success mt-4 disabled">Installed</a>
+              <a href="#" class="btn btn-success mt-4 disabled">{{ __('admin.installed') }}</a>
             @endif
         </div>
         @endforeach
@@ -106,19 +106,19 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="installUpdateModalLabel">Installing Update {{ $latest_version->version }}</h5>
+        <h5 class="modal-title" id="installUpdateModalLabel">{{ __('admin.installing_update') }} {{ $latest_version->version }}</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
         <div class="alert alert-info" role="alert">
-          It's important that you create a backup of files as well as the database if an unforeseen event is to happen.
+          {{ __('admin.installing_update_warn') }}
         </div>
         {!! $latest_version->changelog !!}
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+        <button type="button" class="btn btn-danger" data-dismiss="modal">{!! __('admin.cancel') !!}</button>
         <a href="{{ route('updates.install', ['version' => $latest_version->version, 'type' => 'stable']) }}" class="btn btn-success">Install {{ $latest_version->version }}</a>
       </div>
     </div>
