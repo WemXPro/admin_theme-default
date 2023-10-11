8636b9afc7f714b84dd6d43de6245834e31f1c2e
GIGABAIT93 <59637348+GIGABAIT93@users.noreply.github.com>
2023-10-11 19:37:51 +0300

    Update index.blade.php

--- overview/index.blade.php
+++ overview/index.blade.php
@@ -210,7 +210,7 @@
 @section('container')
     <section class="section">
         <div class="row">
-            <div class="col-lg-4 col-md-4 col-sm-12">
+            <div class="col-lg-3 col-md-3 col-sm-12">
                 <div class="card card-statistic-2">
                     <div class="card-stats">
                         <div class="card-stats-title">{!! __('admin.orders', ['default' => 'Orders']) !!}
@@ -246,7 +246,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-4 col-sm-12">
+            <div class="col-lg-3 col-md-3 col-sm-12">
                 <div class="card card-statistic-2">
                     <div class="card-chart">
                         <div class="chartjs-size-monitor"
@@ -269,7 +269,7 @@
                     </div>
                     <div class="card-wrap">
                         <div class="card-header">
-                            <h4>{!! __('admin.revenue', ['default' => 'Revenue']) !!}</h4>
+                            <h4>{!! __('admin.revenue') !!}</h4>
                         </div>
                         <div class="card-body">
                             {{ currency('symbol') }}{{ number_format(collect($paid_amounts)->sum(), 2) }}
@@ -277,7 +277,38 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-4 col-sm-12">
+            <div class="col-lg-3 col-md-3 col-sm-12">
+                <div class="card card-statistic-2">
+                    <div class="card-chart">
+                        <div class="chartjs-size-monitor"
+                             style="position: absolute; inset: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;">
+                            <div class="chartjs-size-monitor-expand"
+                                 style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
+                                <div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div>
+                            </div>
+                            <div class="chartjs-size-monitor-shrink"
+                                 style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
+                                <div style="position:absolute;width:200%;height:200%;left:0; top:0"></div>
+                            </div>
+                        </div>
+                        <canvas id="balance-chart" height="80" width="339"
+                                style="display: block; width: 339px; height: 80px;"
+                                class="chartjs-render-monitor"></canvas>
+                    </div>
+                    <div class="card-icon shadow-primary bg-primary">
+                        <i class="fas fa-dollar-sign"></i>
+                    </div>
+                    <div class="card-wrap">
+                        <div class="card-header">
+                            <h4>{!! __('client.subscription') !!}</h4>
+                        </div>
+                        <div class="card-body">
+                            {{ currency('symbol') }}{{ $averageSubscription }}/{!! __('admin.monthly') !!}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-12">
                 <div class="card card-statistic-2">
                     <div class="card-chart">
                         <div class="chartjs-size-monitor"
