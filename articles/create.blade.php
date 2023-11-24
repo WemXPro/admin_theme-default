121dab9f075cdcee10e117ae0df0e83cf811e039
Nabeel Mehdi Imrani <nabeelemraane09@gmail.com>
2023-11-24 18:59:14 +0500

    Update create.blade.php

--- articles/create.blade.php
+++ articles/create.blade.php
@@ -1,6 +1,6 @@
 @extends(AdminTheme::wrapper(), ['title' => __('admin.pages'), 'keywords' => 'WemX Dashboard, WemX Panel'])
 @section('css_libraries')
-    <link rel="stylesheet" href="{{ asset(AdminTheme::assets('modules/summernote/summernote-bs4.css')) }}"/>
+    <link rel="stylesheet" href="{{ asset(AdminTheme::assets('modules/summernote/summernote-bs4.css')) }}" />
     <link rel="stylesheet" href="{{ asset(AdminTheme::assets('modules/select2/dist/css/select2.min.css')) }}">
 @endsection
 
@@ -31,21 +31,20 @@
                                     <option value="unlisted">{{ __('admin.unlisted') }}</option>
                                 </select>
                                 @error('placement')
-                                <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
                                 @enderror
                             </div>
 
                             <div class="form-group col-md-12 col-12">
                                 <label for="title">{!! __('admin.title', ['default' => 'Title']) !!}</label>
                                 <input type="text" oninput="updatePath()" name="title" id="title"
-                                       class="form-control @error('title') is-invalid @enderror" value=""
-                                       required>
+                                    class="form-control @error('title') is-invalid @enderror" value="" required>
                                 @error('title')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
                                 @enderror
                             </div>
 
@@ -58,23 +57,23 @@
                                         </div>
                                     </div>
                                     <input type="text" name="path" id="path" placeholder="terms-of-service"
-                                           class="form-control @error('link') is-invalid @enderror" value="" required/>
+                                        class="form-control @error('link') is-invalid @enderror" value="" required />
                                     @error('path')
-                                    <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
                                     @enderror
                                 </div>
                             </div>
 
                             <div class="form-group col-md-12 col-12">
                                 <label for="content">{!! __('admin.content', ['default' => 'Content']) !!}</label>
-                                <textarea class="summernote form-control @error('description') is-invalid @enderror" name="content"
-                                          id="content" style="height: 200px !important"></textarea>
+                                <textarea class="summernote form-control @error('description') is-invalid @enderror" name="content" id="content"
+                                    style="height: 200px !important"></textarea>
                                 @error('content')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
                                 @enderror
                             </div>
 
@@ -83,22 +82,22 @@
                                 <select
                                     class="form-control select2 select2-hidden-accessible  @error('labels') is-invalid @enderror"
                                     name="labels[]" multiple="" tabindex="-1" aria-hidden="true">
-                                    @foreach(config('article.labels') as $key => $label)
+                                    @foreach (config('article.labels') as $key => $label)
                                         <option value="{{ $key }}">{{ $label['name'] }}</option>
                                     @endforeach
                                 </select>
                                 @error('placement')
-                                <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
                                 @enderror
                             </div>
 
                             <div class="form-group col-md-4 col-12">
-                                <div
-                                    class="control-label">{!! __('admin.allow_guests', ['default' => 'Allow Guests']) !!}</div>
+                                <div class="control-label">{!! __('admin.allow_guests', ['default' => 'Allow Guests']) !!}</div>
                                 <label class="custom-switch mt-2">
-                                    <input type="checkbox" name="allow_guests" class="custom-switch-input" value="1" checked>
+                                    <input type="checkbox" name="allow_guests" class="custom-switch-input" value="1"
+                                        checked>
                                     <span class="custom-switch-indicator"></span>
                                     <span class="custom-switch-description">
                                         {!! __('admin.page_allow_guests_desc', ['default' => 'Determine whether the page is viewable for guests']) !!}
@@ -107,23 +106,22 @@
                             </div>
 
                             <div class="form-group col-md-4 col-12">
-                                <div
-                                    class="control-label">{{ __('admin.show_author') }}</div>
+                                <div class="control-label">{{ __('admin.show_author') }}</div>
                                 <label class="custom-switch mt-2">
                                     <input type="checkbox" name="show_author" class="custom-switch-input" value="1"
-                                           checked>
+                                        checked>
                                     <span class="custom-switch-indicator"></span>
                                     <span class="custom-switch-description">
-                                    {{ __('admin.show_author_desc') }}
-                              </span>
+                                        {{ __('admin.show_author_desc') }}
+                                    </span>
                                 </label>
                             </div>
 
                             <div class="form-group col-md-4 col-12">
-                                <div
-                                    class="control-label">{{ __('admin.allow_comments') }}</div>
+                                <div class="control-label">{{ __('admin.allow_comments') }}</div>
                                 <label class="custom-switch mt-2">
-                                    <input type="checkbox" name="allow_comments" class="custom-switch-input" value="1" checked="">
+                                    <input type="checkbox" name="allow_comments" class="custom-switch-input" value="1"
+                                        checked="">
                                     <span class="custom-switch-indicator"></span>
                                     <span class="custom-switch-description">
                                         {{ __('admin.allow_comments_desc') }}
@@ -145,11 +143,11 @@
             var path = document.getElementById('path');
             var title = document.getElementById('title').value;
             path.value = title
-                        .toLowerCase() // convert to lowercase
-                        .trim() // remove leading and trailing whitespace
-                        .replace(/[^\w\s-]/g, '') // remove non-word characters
-                        .replace(/[\s_-]+/g, '-') // replace spaces, underscores, and hyphens with a single hyphen
-                        .replace(/^-+|-+$/g, ''); // remove leading and trailing hyphens
+                .toLowerCase() // convert to lowercase
+                .trim() // remove leading and trailing whitespace
+                .replace(/[^\w\s-]/g, '') // remove non-word characters
+                .replace(/[\s_-]+/g, '-') // replace spaces, underscores, and hyphens with a single hyphen
+                .replace(/^-+|-+$/g, ''); // remove leading and trailing hyphens
         }
     </script>
 @endsection
