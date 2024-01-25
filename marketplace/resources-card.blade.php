1de5feb93a0300e02ee46fc7cb5978d829367ec7
GIGABAIT93 <59637348+GIGABAIT93@users.noreply.github.com>
2024-01-25 08:23:27 +0200

    Update resources-card.blade.php

--- marketplace/resources-card.blade.php
+++ marketplace/resources-card.blade.php
@@ -1,80 +1,66 @@
-@if(isset($marketplace) and count($marketplace))
-<div class="card">
-    <div class="card-header">
-        <h4>{!! __('admin.marketplace') !!} Beta</h4>
-    </div>
-    <div class="card-body">
-        <div class="table-responsive">
-            <table class="table table-striped">
-                <thead>
-                <tr>
-                    <th>{!! __('admin.name') !!}</th>
-                    <th>{!! __('admin.description') !!}</th>
-                    <th>{!! __('admin.author') !!}</th>
-                    <th>{!! __('admin.version') !!}</th>
-                    <th>{!! __('admin.wemx_version') !!}</th>
-                    <th>{!! __('admin.price') !!}</th>
-                    <th class="text-right">{!! __('admin.actions') !!}</th>
-                </tr>
-                </thead>
-                <tbody>
-                @php($install_key = 'install')
-                @foreach($marketplace['data'] as $resource)
-                    @if($installedResource = Module::find($resource['real_name']))
-                        @php($resource['installed'] = true)
-                        @php($install_key = 'reinstall')
-                        @php($installedResourceConfig = config($installedResource->getLowerName()))
-                    @else
-                        @php($resource['installed'] = false)
-                        @php($install_key = 'install')
-                    @endif
-
+@if($marketplace && count($marketplace))
+    <div class="card">
+        <div class="card-header">
+            <h4>{!! __('admin.marketplace') !!}</h4>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-striped">
+                    <thead>
                     <tr>
-                        <td>
-                            <img src="{{ $resource['icon'] ?? 'https://imgur.com/koz9j8a.png' }}" alt="Icon"
-                                 style="width:32px; height:32px;">
-                            {{ $resource['name'] }}
-                        </td>
-
-                        <td>@if(!empty($resource['short_desc']))
-                                {{ $resource['short_desc'] }}
-                            @else
-                                {{ $resource['name'] }}
-                            @endif</td>
-                        <td>
-                            <img src="{{ $resource['owner']['avatar'] ?? 'https://imgur.com/koz9j8a.png' }}"
-                                 alt="Icon" style="width:32px; height:32px;">
-                            {{ $resource['owner']['username'] }}
-                        </td>
-                        <td>{{ $resource['version'] }}</td>
-                        <td>
-                            @foreach($resource['wemx_version'] as $wemx_version)
-                                {{ $wemx_version }}
-                            @endforeach
-                        </td>
-                        <td>@if($resource['is_free'])
-                                {!! __('admin.free') !!}
-                            @else
-                                {{ $resource['price'] }}
-                            @endif</td>
-                        <td class="text-right">
-                            @if($resource['purchased'])
-                                <a href="{{ route('admin.resource.install', ['resource_id' => $resource['id'], 'version_id' => $resource['version_id']]) }}"
-                                   class="btn btn-primary">
-                                    {!! __('admin.'.$install_key) !!}
-                                </a>
-                            @endif
-                            <a href="{{ $resource['view_url'] }}"
-                               class="btn btn-success">
-                                {!! __('admin.view') !!}
-                            </a>
-
-                        </td>
+                        <th>{!! __('admin.name') !!}</th>
+                        <th>{!! __('admin.description') !!}</th>
+                        <th>{!! __('admin.author') !!}</th>
+                        <th>{!! __('admin.version') !!}</th>
+                        <th>{!! __('admin.wemx_version') !!}</th>
+                        <th>{!! __('admin.price') !!}</th>
+                        <th class="text-right">{!! __('admin.actions') !!}</th>
                     </tr>
-                @endforeach
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                    @foreach($marketplace['data'] as $resource)
+                        @php
+                            $installedResource = Module::find($resource['real_name']);
+                            $resource['installed'] = (bool)$installedResource;
+                            $install_key = $resource['installed'] ? 'reinstall' : 'install';
+                            if($resource['installed']) {
+                                $config = require $installedResource->getExtraPath('Config/config.php');
+                                if (version_compare($config['version'], $resource['version'], '<')){
+                                    $install_key = 'update';
+                                }
+                            }
+                        @endphp
+
+                        <tr>
+                            <td>
+                                <img src="{{ $resource['icon'] ?? 'https://imgur.com/koz9j8a.png' }}"
+                                     alt="{{ $resource['name'] }}" style="width:32px; height:32px;">
+                                {{ $resource['name'] }}
+                            </td>
+                            <td>{{ Str::limit($resource['short_desc'] ?? $resource['name'], 50) }}</td>
+                            <td>
+                                <img src="{{ $resource['owner']['avatar'] ?? 'https://imgur.com/koz9j8a.png' }}"
+                                     alt="{{ $resource['name'] }}" style="width:32px; height:32px;">
+                                {{ $resource['owner']['username'] }}
+                            </td>
+                            <td>{{ $resource['version'] }}</td>
+                            <td>{{ implode(', ', $resource['wemx_version']) }}</td>
+                            <td>{{ $resource['is_free'] ? __('admin.free') : $resource['price'] }}</td>
+                            <td class="text-right">
+                                @if($resource['purchased'])
+                                    <a href="{{ route('admin.resource.install', ['resource_id' => $resource['id'], 'version_id' => $resource['version_id']]) }}"
+                                       class="btn btn-primary">
+                                        {!! __('admin.'.$install_key) !!}
+                                    </a>
+                                @endif
+                                <a href="{{ $resource['view_url'] }}"
+                                   class="btn btn-success">{!! __('admin.view') !!}</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
-</div>
 @endif
