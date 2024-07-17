0fd5e03f8146288c1a965b98c89b3d2173aa54b1
Mubeen142 <mubeenullah323@gmail.com>
2024-07-17 21:30:20 +0200

    Update config-options.blade.php

--- packages/edit/config-options.blade.php
+++ packages/edit/config-options.blade.php
@@ -218,7 +218,7 @@
                 <label for="key">{{ __('admin.key') }}</label>
                 <select class="form-control select2 select2-hidden-accessible hide" name="key" tabindex="-1" aria-hidden="true">
                     @foreach($package->service()->getPackageConfig($package)->all() as $config)
-                        @if(isset($config['is_configurable']) AND !$config['is_configurable'])
+                        @if(!isset($config['is_configurable']) OR !$config['is_configurable'])
                             @continue
                         @endif
                         <option value="{{ $config['key'] }}">{{ $config['name'] }} ({{ $config['type'] }})</option>
