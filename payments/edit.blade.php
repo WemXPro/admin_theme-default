22c344bdcf89aad17197deb38c84382c328bb665
GIGABAIT93 <59637348+GIGABAIT93@users.noreply.github.com>
2025-01-13 11:39:51 +0200

    Update edit.blade.php

--- payments/edit.blade.php
+++ payments/edit.blade.php
@@ -49,7 +49,8 @@
                             {!! __('admin.user') !!}
                         </a>
                         @isset($payment->order_id)
-                            <a href="{{ route('orders.edit', $payment->order_id) }}" class="btn btn-icon icon-left btn-dark"><i
+                            <a href="{{ route('orders.edit', $payment->order_id) }}"
+                               class="btn btn-icon icon-left btn-dark"><i
                                     class="fas fa-solid fa-server"></i>
                                 {!! __('admin.order') !!}
                             </a>
@@ -60,7 +61,8 @@
                             {!! __('admin.invoice') !!}
                         </a>
                         @if($payment->status == 'unpaid')
-                            <button type="button" data-toggle="modal" data-target="#CompletePaymentModal" class="btn btn-icon icon-left btn-dark"><i
+                            <button type="button" data-toggle="modal" data-target="#CompletePaymentModal"
+                                    class="btn btn-icon icon-left btn-dark"><i
                                     class="fas fa-solid fa-circle-check"></i>
                                 {!! __('admin.complete_payment', ['default' => 'Complete Payment']) !!}
                             </button>
@@ -78,20 +80,24 @@
                                 <div class="col-8 mb-3">
                                     <label
                                         for="currency">{!! __('admin.gateway_name', ['default' => 'Gateway Name']) !!}</label>
-                                    <input type="text" class="form-control" value="{{ $payment->gateway['name'] ?? '' }}"
+                                    <input type="text" class="form-control"
+                                           value="{{ $payment->gateway['name'] ?? '' }}"
                                            disabled="">
                                 </div>
 
                                 <div class="col-4 mb-3">
                                     <label for="currency">{!! __('admin.type', ['default' => 'Type']) !!}</label>
-                                    <input type="text" class="form-control" value="{{ $payment->gateway['type'] ?? '' }}"
+                                    <input type="text" class="form-control"
+                                           value="{{ $payment->gateway['type'] ?? '' }}"
                                            disabled="">
                                 </div>
 
                                 <div class="col-12 mb-3">
-                                    <label for="transaction_id">{!! __('admin.transaction_id', ['default' => 'Transaction ID']) !!}</label>
+                                    <label
+                                        for="transaction_id">{!! __('admin.transaction_id', ['default' => 'Transaction ID']) !!}</label>
                                     <input type="text" class="form-control" name="transaction_id"
-                                           value="{{ $payment->transaction_id }}" placeholder="{!! __('admin.transaction_id', ['default' => 'Transaction ID']) !!}">
+                                           value="{{ $payment->transaction_id }}"
+                                           placeholder="{!! __('admin.transaction_id', ['default' => 'Transaction ID']) !!}">
                                 </div>
                             </div>
                             <!-- Button trigger modal -->
@@ -176,6 +182,11 @@
                             <textarea type="text" class="form-control" rows="4"
                                       name="notes">{{ $payment->notes }}</textarea>
                         </div>
+                        <div class="form-group col-md-12 col-12">
+                            <label> {{ __('admin.data') }} - {{ $payment?->gateway['driver'] ?? __('admin.gateways') }}</label>
+                            <textarea class="form-control" style="min-height: 200px;" disabled>{{ json_encode($payment->data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE) }}</textarea>
+                        </div>
+
                     </div>
 
                 </div>
@@ -188,44 +199,50 @@
     </div>
 
     <!-- Complete Payment Modal -->
-    <div class="modal fade" id="CompletePaymentModal" tabindex="-1" role="dialog" aria-labelledby="CompletePaymentModalLabel" aria-hidden="true">
+    <div class="modal fade" id="CompletePaymentModal" tabindex="-1" role="dialog"
+         aria-labelledby="CompletePaymentModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-            <h5 class="modal-title" id="CompletePaymentModalLabel">Complete Payment</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-            </div>
-            <form action="{{ route('payments.complete', $payment->id) }}" method="POST">
-                @csrf
-                <div class="modal-body">
-                    <p>You can manually complete payments that are unpaid. On completion, the application will attempt to mark the payment as complete and run the program the payment was created for.</p>
-
-                    <div class="form-group">
-                        <label for="user">{!! __('client.payment_method') !!}</label>
-                        <select class="form-control select2 select2-hidden-accessible" name="gateway" tabindex="-1"
-                                aria-hidden="true">
-                            @foreach (App\Models\Gateways\Gateway::getActive('subscription') as $gateway)
-                                <option value="{{ $gateway->id }}" @if(isset($payment->gateway['id']) AND $payment->gateway['id'] == $gateway->id) selected @endif>{{ $gateway->name }} ({!! __('client.subscription') !!})</option>
-                            @endforeach
-                            @foreach (App\Models\Gateways\Gateway::getActive() as $gateway)
-                                <option value="{{ $gateway->id }}" @if(isset($payment->gateway['id']) AND $payment->gateway['id'] == $gateway->id) selected @endif>{{ $gateway->name }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-
-                    <div class="form-group">
-                        <label>Transaction ID (optional)</label>
-                        <input type="text" class="form-control" name="transaction_id" value="">
-                    </div>
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="CompletePaymentModalLabel">Complete Payment</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
+                <form action="{{ route('payments.complete', $payment->id) }}" method="POST">
+                    @csrf
+                    <div class="modal-body">
+                        <p>You can manually complete payments that are unpaid. On completion, the application will
+                            attempt to mark the payment as complete and run the program the payment was created for.</p>
+
+                        <div class="form-group">
+                            <label for="user">{!! __('client.payment_method') !!}</label>
+                            <select class="form-control select2 select2-hidden-accessible" name="gateway" tabindex="-1"
+                                    aria-hidden="true">
+                                @foreach (App\Models\Gateways\Gateway::getActive('subscription') as $gateway)
+                                    <option value="{{ $gateway->id }}"
+                                            @if(isset($payment->gateway['id']) AND $payment->gateway['id'] == $gateway->id) selected @endif>{{ $gateway->name }}
+                                        ({!! __('client.subscription') !!})
+                                    </option>
+                                @endforeach
+                                @foreach (App\Models\Gateways\Gateway::getActive() as $gateway)
+                                    <option value="{{ $gateway->id }}"
+                                            @if(isset($payment->gateway['id']) AND $payment->gateway['id'] == $gateway->id) selected @endif>{{ $gateway->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <label>Transaction ID (optional)</label>
+                            <input type="text" class="form-control" name="transaction_id" value="">
+                        </div>
+                    </div>
                     <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary">Complete Payment</button>
-                </div>
-            </form>
-        </div>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Complete Payment</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 
@@ -236,7 +253,8 @@
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="refundModalLabel">{!! __('admin.refund_payment', ['default' => 'Refund Payment']) !!}</h5>
+                        <h5 class="modal-title"
+                            id="refundModalLabel">{!! __('admin.refund_payment', ['default' => 'Refund Payment']) !!}</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
@@ -280,9 +298,11 @@
                             @endif
                         </div>
                         <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">{!! __('admin.close') !!}</button>
+                            <button type="button" class="btn btn-secondary"
+                                    data-dismiss="modal">{!! __('admin.close') !!}</button>
                             @if(isset($payment->gateway['refund_support']) AND $payment->gateway['refund_support'] AND $payment->status == 'paid')
-                                <button type="submit" class="btn btn-primary">{!! __('admin.submit_refund', ['default' => 'Submit Refund']) !!}</button>
+                                <button type="submit"
+                                        class="btn btn-primary">{!! __('admin.submit_refund', ['default' => 'Submit Refund']) !!}</button>
                         @endif
                     </form>
                 </div>
